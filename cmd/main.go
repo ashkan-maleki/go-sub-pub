ab63e19783c939b2cd0c45dde56195b995b59700
mamalmaleki <mohammadalimalekie@gmail.com>
2023-05-20 19:17:16 +0330

    initiate projects

---
+++ cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+	// TODO: connect to database
+
+	// TODO: create sessions
+
+	// TODO: create channels
+
+	// TODO: create wait group
+
+	// TODO: set up the application logic
+
+	// TODO: set up mail
+
+	// TODO: listen for web connections
+
+}
